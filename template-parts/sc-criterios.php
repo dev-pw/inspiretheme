4040a5d4bb3985838525ae5f90bbe1c6b0ebf4eb
Brandão <lucas.brandao@planetaw.ag>
2024-10-31 17:42:13 -0300

    Ajustes dos cards

--- template-parts/sc-criterios.php
+++ template-parts/sc-criterios.php
@@ -14,26 +14,26 @@
                 <div class="row">
                     <div class="col-md-6 col-lg-3">
                         <div class="bg-secondary rounded-5 p-5">
-                            <i class="icon-instagram"></i>
-                            <p>Idade maior ou igual a 18 anos</p>
+                            <i class="icon-instagram fs-1"></i>
+                            <p class="fs-5 text-white">Idade maior ou igual a 18 anos</p>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-3">
-                        <div class="bg-primary p-rounded-5 5">
-                            <p>Tabagismo ativo, incluindo cigarro eletrônico</p>
-                            <i class="icon-instagram"></i>
+                        <div class="bg-primary h-100 rounded-5 p-5">
+                            <p class="fs-5 text-white">Tabagismo ativo, incluindo cigarro eletrônico</p>
+                            <i class="icon-instagram fs-1"></i>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-3">
                         <div class="bg-secondary rounded-5 p-5">
-                            <i class="icon-instagram"></i>
-                            <p>Ex-fumante há menos que 15 anos</p>
+                            <i class="icon-instagram fs-1"></i>
+                            <p class="fs-5 text-white">Ex-fumante há menos que 15 anos</p>
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-3">
-                        <div class="bg-primary p-rounded-5 5">
-                            <p>Sintomas respiratórios crônicos</p>
-                            <i class="icon-instagram"></i>
+                        <div class="bg-primary h-100 rounded-5 p-5">
+                            <p class="fs-5 text-white">Sintomas respiratórios crônicos</p>
+                            <i class="icon-instagram fs-1"></i>
                         </div>
                     </div>
                 </div>
