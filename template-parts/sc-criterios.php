5962e5c0571085de7642d454175237e83553a5c5
Brandão <lucas.brandao@planetaw.ag>
2024-10-31 17:33:06 -0300

    Ajustes dos cards

--- template-parts/sc-criterios.php
+++ template-parts/sc-criterios.php
@@ -1,6 +1,6 @@
 <section class="l-criterios">
     <div class="container u-py-5">
-        <div class="row">
+        <div class="row gy-5">
             <div class="col-lg-6">
                 <h2 class="text-end">Critérios para <br> inclusão no programa</h2>
             </div>
@@ -12,26 +12,26 @@
             </div>
             <div class="col-12">
                 <div class="row">
-                    <div class="col-md-6 col-lg-4">
-                        <div class="bg-secondary p-5">
+                    <div class="col-md-6 col-lg-3">
+                        <div class="bg-secondary rounded-5 p-5">
                             <i class="icon-instagram"></i>
                             <p>Idade maior ou igual a 18 anos</p>
                         </div>
                     </div>
-                    <div class="col-md-6 col-lg-4">
-                        <div class="bg-secondary p-5">
+                    <div class="col-md-6 col-lg-3">
+                        <div class="bg-primary p-rounded-5 5">
                             <p>Tabagismo ativo, incluindo cigarro eletrônico</p>
                             <i class="icon-instagram"></i>
                         </div>
                     </div>
-                    <div class="col-md-6 col-lg-4">
-                        <div class="bg-secondary p-5">
+                    <div class="col-md-6 col-lg-3">
+                        <div class="bg-secondary rounded-5 p-5">
                             <i class="icon-instagram"></i>
                             <p>Ex-fumante há menos que 15 anos</p>
                         </div>
                     </div>
-                    <div class="col-md-6 col-lg-4">
-                        <div class="bg-secondary p-5">
+                    <div class="col-md-6 col-lg-3">
+                        <div class="bg-primary p-rounded-5 5">
                             <p>Sintomas respiratórios crônicos</p>
                             <i class="icon-instagram"></i>
                         </div>
